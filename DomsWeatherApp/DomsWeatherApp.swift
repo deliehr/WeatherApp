48d17f27ac7039c431ecad7b634bfc8a74e5c853
Dominik Liehr <dominik_liehr@me.com>
2024-05-26 17:49:51 +0200

    Refactor: removed swift data

---
+++ DomsWeatherApp/DomsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomsWeatherApp.swift
+//  DomsWeatherApp
+//
+//  Created by Dominik Liehr on 26.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DomsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
