133ef68c7d66741e3b728f8bf7c467569d74dfea
Dominik Liehr <dominik_liehr@me.com>
2024-05-26 22:26:13 +0200

    Feat: included api key for weather requests

---
+++ DomsWeatherApp/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  DomsWeatherApp
+//
+//  Created by Dominik Liehr on 26.05.24.
+//
+
+import Foundation
+
+class Constants {
+    private init() {}
+
+    static var apiKey: String? { weatherApiKey }
+}
